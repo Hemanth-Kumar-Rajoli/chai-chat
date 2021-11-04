c3f66067fa8a5b4e1c2c0aa5baf8c77aadcbb4ff
Hemanth Kumar Rajoli <hemanthkumarrajoli@gmail.com>
2021-11-04 20:23:12 +0530

    added send grid

--- utils/email.js
+++ utils/email.js
@@ -1,12 +1,19 @@
 const nodeMailer = require('nodemailer');
 const checkAsync = require('./erroHandilings/checkAsync');
 const sendEmail = async(options)=>{
+    // const traspoter = nodeMailer.createTransport({
+    //     host:process.env.MAIL_TRAP_HOST,
+    //     port:process.env.MAIL_TRAP_PORT,
+    //     auth:{
+    //         user:process.env.MAIL_TRAP_USER_NAME,
+    //         pass:process.env.MAIL_TRAP_USER_PASSWORD
+    //     }
+    // })
     const traspoter = nodeMailer.createTransport({
-        host:process.env.MAIL_TRAP_HOST,
-        port:process.env.MAIL_TRAP_PORT,
+        service:'SendGrid',
         auth:{
-            user:process.env.MAIL_TRAP_USER_NAME,
-            pass:process.env.MAIL_TRAP_USER_PASSWORD
+            user:process.env.NODE_MIALER_USER_NAME,
+            pass:process.env.NODE_MAILER_USER_PASSWORD
         }
     })
     const mailOptions={
